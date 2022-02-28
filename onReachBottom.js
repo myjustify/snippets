66f5471db8907617fcf56e52e18aa5d44aca3fb8
myjustify <1406023741@qq.com>
2022-02-28 14:40:01 +0800

    Update onReachBottom.js

--- onReachBottom.js
+++ onReachBottom.js
@@ -1,52 +1,40 @@
-(function (target){
-    function onReachBottom(options){
-        if(!options) return
-        this.status = false
-        this.scrollTop = 0
-        this.bottomDistance = options.bottomDistance || 10
-        this.el = options.el||document.documentElement || document.body
-        this.handleScroll = (function () {
-            if(this.status) return;
-            const scrollTop = this.el.scrollTop
-            const clientHeight = this.el.clientHeight
-            const scrollHeight = this.el.scrollHeight
-            const flag = scrollTop > this.scrollTop
-            this.scrollTop = scrollTop
-            if (flag && scrollTop + clientHeight >= scrollHeight - this.bottomDistance) {
-                // 触底后回滚 scrollTop 反而增加3到4
-                this.scrollTop = scrollTop + 4
-                if(options.success) {
-                    this.throttle(options.success)
-                }
-            }
-        }).bind(this)
-
-        this.throttle = function (fn, delay) {
-            delay = delay || 200
-            if(this.status) return
-            this.status = true
-            setTimeout(() => {
-                this.status = false
-            },delay)
-            fn()
-        }
-
-        this.open = function () {
-            target.addEventListener('scroll', this.handleScroll, true)
-        }
-
-        this.close = function () {
-            target.removeEventListener('scroll', this.handleScroll)
-        }
+export default function onReachBottom(options) {
+  if (!options) return
+  this.status = false
+  this.scrollTop = 0
+  this.bottomDistance = options.bottomDistance || 10
+  this.el = options.el || document.documentElement || document.body
+  this.handleScroll = function() {
+    if (this.status) return
+    const scrollTop = this.el.scrollTop
+    const clientHeight = this.el.clientHeight
+    const scrollHeight = this.el.scrollHeight
+    const flag = scrollTop > this.scrollTop
+    this.scrollTop = scrollTop
+    if (flag && scrollTop + clientHeight >= scrollHeight - this.bottomDistance) {
+      // 触底后回滚 scrollTop 反而增加3到4
+      this.scrollTop = scrollTop + 4
+      if (options.success) {
+        this.throttle(options.success)
+      }
     }
-    target.onReachBottom = onReachBottom
-})(window)
+  }.bind(this)
 
-var scrollCtx = new onReachBottom({
-    success: function () {
-        console.log(11111)
-    }
-})
+  this.throttle = function(fn, delay) {
+    delay = delay || 500
+    if (this.status) return
+    this.status = true
+    setTimeout(() => {
+      this.status = false
+    }, delay)
+    fn()
+  }
 
-scrollCtx.open()
-// scrollCtx.close()
+  this.open = function() {
+    this.el.addEventListener('scroll', this.handleScroll, true)
+  }
+
+  this.close = function() {
+    this.el.removeEventListener('scroll', this.handleScroll)
+  }
+}
